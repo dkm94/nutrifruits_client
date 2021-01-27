6edc10be7236151dff81d154cac62d50f1b3b70d
dkm94 <diane.mpacko@gmail.com>
2021-01-27 07:58:45 +0100

    nutrifruits desktop v2

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import Cards from "../Card/Cards";
 import Cards from "../Card/Cards2";
-import sort from "../../img/iconsFilter.png";
 import SearchBar from "../SearchBar/SearchBar";
 
 import "../Filter/filter.css";
@@ -30,107 +28,53 @@ class Filter extends React.Component {
       });
   }
 
-  ascProteins = () => {
-    const fruits = this.state.fruits;
-    fruits.sort(function (a, b) {
-      return a.nutritions.protein - b.nutritions.protein;
-    });
-    this.setState({ ...[fruits] });
-  };
-
-  ascFat = () => {
-    const fruits = this.state.fruits;
-    fruits.sort(function (a, b) {
-      return a.nutritions.fat - b.nutritions.fat;
-    });
-    this.setState({ fruits });
-  };
-
-  ascCalories = () => {
-    const fruits = this.state.fruits;
-    fruits.sort(function (a, b) {
-      return a.nutritions.calories - b.nutritions.calories;
-    });
-    this.setState({ fruits });
-  };
-
-  ascSugar = () => {
-    const fruits = this.state.fruits;
-    fruits.sort(function (a, b) {
-      return a.nutritions.sugar - b.nutritions.sugar;
-    });
-    this.setState({ fruits });
-  };
-
   inputValue = (e) => {
     e.preventDefault();
     this.setState({ value: e.target.value });
   };
 
+  sorting = (e) => {
+    const condition = e.target.value;
+    const fruits = this.state.fruits;
+
+    const result = fruits.sort((a,b)=>{
+      if(condition === 'proteins'){
+        return a.nutritions.protein - b.nutritions.protein;
+      }
+      if(condition === 'fat'){
+        return a.nutritions.fat - b.nutritions.fat;
+      }
+      if(condition === 'calories'){
+        return a.nutritions.calories - b.nutritions.calories;
+      }
+      if(condition === 'sugar'){
+        return a.nutritions.sugar - b.nutritions.sugar;
+      } else {
+          return a.name > b.name ? 1 : - 1
+        }
+      }
+    )
+    this.setState({ fruits: result });
+  }
+
   render() {
     return (
       <>
         <div className="filter">
-          <div className="sort">
-            <img alt="trier" src={sort} />
-            <h1>Sort in ascending order :</h1>
-          </div>
-
-          <div className="inputFilter">
-            <div className="inputContainer">
-              <label>
-                <input
-                  type="radio"
-                  className="option-input radio"
-                  name="example"
-                  onClick={this.ascProteins}
-                />
-                Proteins
-              </label>
-            </div>
-
-            <div className="inputContainer">
-              <label>
-                <input
-                  type="radio"
-                  className="option-input radio"
-                  name="example"
-                  onClick={this.ascFat}
-                />
-                Fat
-              </label>
-            </div>
-
-            <div className="inputContainer">
-              <label>
-                <input
-                  type="radio"
-                  className="option-input radio"
-                  name="example"
-                  onClick={this.ascCalories}
-                />
-                Calories
-              </label>
-            </div>
-
-            <div className="inputContainer">
-              <label>
-                <input
-                  type="radio"
-                  className="option-input radio"
-                  name="example"
-                  onClick={this.ascSugar}
-                />
-                Sugar
-              </label>
-            </div>
-          </div>
+          <label htmlFor="nutritient-select">Sort in ascending order:</label>
+          <select name="nutritient" onChange={this.sorting}>
+            <option value="alphabetical">Alphabetical</option>
+            <option value="proteins">Proteins</option>
+            <option value="fat">Fat</option>
+            <option value="calories">Calories</option>
+            <option value="sugar">Sugar</option>
+          </select>
         </div>
 
         <SearchBar
           className="search__input"
           type="text"
-          placeholder="Search your fruit"
+          placeholder="Search a fruit"
           name="searchbar"
           onChange={this.inputValue}
           // value={this.state.value}
