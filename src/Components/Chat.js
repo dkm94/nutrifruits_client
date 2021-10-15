1dd2b954492e8d88e643dd6e91ad720f6c4950b6
dkm94 <diane.mpacko@gmail.com>
2021-10-15 03:24:45 +0200

    update folder

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -13,7 +13,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const [users, setUsers] = useState("");
+  // const [users, setUsers] = useState("");
 
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
