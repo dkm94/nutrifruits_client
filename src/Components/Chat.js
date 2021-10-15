52f10ad27a7f6b6c3424db47e65b8d211923f9db
dkm94 <diane.mpacko@gmail.com>
2021-10-15 03:35:17 +0200

    comment /Chat

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,98 +1,98 @@
-import React, { useState, useEffect } from "react";
-import queryString from "query-string";
-import io from "socket.io-client";
-import "./Chat.css";
-import InfoBar from "./InfoBar/InfoBar";
-import Messages from "./Messages/Messages";
+// import React, { useState, useEffect } from "react";
+// import queryString from "query-string";
+// import io from "socket.io-client";
+// import "./Chat.css";
+// import InfoBar from "./InfoBar/InfoBar";
+// import Messages from "./Messages/Messages";
 
-import Input from "./Input/Input";
-import { withRouter } from "react-router-dom";
+// import Input from "./Input/Input";
+// import { withRouter } from "react-router-dom";
 
-let socket;
+// let socket;
 
-const Chat = ({ location }) => {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
-  // const [users, setUsers] = useState("");
+// const Chat = ({ location }) => {
+//   const [name, setName] = useState("");
+//   const [room, setRoom] = useState("");
+//   // const [users, setUsers] = useState("");
 
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  // const ENDPOINT = 'http://localhost:3030';
-  const ENDPOINT = "https://nutrifruits.netlify.app/";
+//   const [message, setMessage] = useState("");
+//   const [messages, setMessages] = useState([]);
+//   // const ENDPOINT = 'http://localhost:3030';
+//   const ENDPOINT = "https://nutrifruits.netlify.app/";
 
-  useEffect(
-    () => {
-      // les data que l'utilisateur a entré dans les queries
-      // une methode du package queryString
-      // const data = queryString.parse(location.search);
-      const { room, name } = queryString.parse(location.search);
+//   useEffect(
+//     () => {
+//       // les data que l'utilisateur a entré dans les queries
+//       // une methode du package queryString
+//       // const data = queryString.parse(location.search);
+//       const { room, name } = queryString.parse(location.search);
 
-      socket = io(ENDPOINT);
+//       socket = io(ENDPOINT);
 
-      setName(name);
-      setRoom(room);
+//       setName(name);
+//       setRoom(room);
 
-      socket.emit("join", { name, room }, () => {});
+//       socket.emit("join", { name, room }, () => {});
 
-      return () => {
-        socket.emit("disconnect");
-        socket.off();
-      };
-    }, //la suite indique quand le useEffect prendra effet donc
-    // quand les valeurs de endpoint et location.search changeront
-    [ENDPOINT, location.search]
-  );
+//       return () => {
+//         socket.emit("disconnect");
+//         socket.off();
+//       };
+//     }, //la suite indique quand le useEffect prendra effet donc
+//     // quand les valeurs de endpoint et location.search changeront
+//     [ENDPOINT, location.search]
+//   );
 
-  useEffect(
-    () => {
-      //ajoute tous les nouveaux messages dans le tableau des messages
-      socket.on("message", (message) => {
-        setMessages([...messages, message]);
-      });
+//   useEffect(
+//     () => {
+//       //ajoute tous les nouveaux messages dans le tableau des messages
+//       socket.on("message", (message) => {
+//         setMessages([...messages, message]);
+//       });
 
-      socket.on("roomData", ({ users }) => {
-        setUsers(users);
-      });
-    },
-    //indique 'seulement comme le tableaux messages change'
-    [messages]
-  );
+//       socket.on("roomData", ({ users }) => {
+//         setUsers(users);
+//       });
+//     },
+//     //indique 'seulement comme le tableaux messages change'
+//     [messages]
+//   );
 
-  //fonction pour envoyer des messages
-  const sendMessage = (event) => {
-    event.preventDefault();
+//   //fonction pour envoyer des messages
+//   const sendMessage = (event) => {
+//     event.preventDefault();
 
-    if (message) {
-      socket.emit("sendMessage", message, () => setMessage(""));
-    }
-  };
+//     if (message) {
+//       socket.emit("sendMessage", message, () => setMessage(""));
+//     }
+//   };
 
-  return (
-    <div className="outerContainer">
-      <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
-      </div>
-      {/* <TextContainer users={users} /> */}
-    </div>
+//   return (
+//     <div className="outerContainer">
+//       <div className="container">
+//         <InfoBar room={room} />
+//         <Messages messages={messages} name={name} />
+//         <Input
+//           message={message}
+//           setMessage={setMessage}
+//           sendMessage={sendMessage}
+//         />
+//       </div>
+//       {/* <TextContainer users={users} /> */}
+//     </div>
 
-    // <div className='outerContainer'>
-    //   <div className='container'>
-    //     <InfoBar />
-    //     <input
-    //       value={messages}
-    //       onChange={(event) => setMessage(event.target.value)}
-    //       // envoi avec la touche entrée
-    //       onKeyPress={(event)=> event.key ==='Enter' ? sendMessage(event) : null}
-    //     />
-    //   </div>
-    // </div>
-  );
-};
+//     // <div className='outerContainer'>
+//     //   <div className='container'>
+//     //     <InfoBar />
+//     //     <input
+//     //       value={messages}
+//     //       onChange={(event) => setMessage(event.target.value)}
+//     //       // envoi avec la touche entrée
+//     //       onKeyPress={(event)=> event.key ==='Enter' ? sendMessage(event) : null}
+//     //     />
+//     //   </div>
+//     // </div>
+//   );
+// };
 
-export default withRouter(Chat);
+// export default withRouter(Chat);
